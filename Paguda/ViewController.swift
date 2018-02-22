15f9df3b96f33b1cb6a2092974d84a0fd801309b
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-22 19:45:20 +0200

    First commit. Initializing repository

---
+++ Paguda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paguda
+//
+//  Created by Manolescu Mihai Alexandru on 22/02/2018.
+//  Copyright © 2018 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
