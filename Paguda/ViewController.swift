0042f90515d2c8bfaf5273c9b7730e7b14cc7cda
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-23 18:25:26 +0200

    I addeda nice gradient alpha effect on the tableView

--- Paguda/ViewController.swift
+++ Paguda/ViewController.swift
@@ -8,22 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    
+    private var gradient = CAGradientLayer()
 
     @IBOutlet var topView: UIView!
     
-
+    @IBOutlet var tbv: UITableView!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        topView.layer.cornerRadius = topView.frame.size.height / 10
+        setUp()
+        tbv.dataSource = self
+        tbv.delegate = self
+        
+        
     }
 
 
+    func setUp()
+    {
+        topView.layer.cornerRadius = topView.frame.size.height / 10
+        
+        
+        
+        gradient.frame = tbv.superview?.bounds ?? CGRect.null
+        //gradient.colors = [UIColor.clear.cgColor, UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor, UIColor.clear.cgColor]
+        gradient.colors = [UIColor.clear.cgColor,  UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor,  UIColor.clear.cgColor]
+        gradient.locations = [0.0, 0.05, 0.15, 0.7, 0.85, 1.0]
+        
+        tbv.superview?.layer.mask = gradient
+        
+        tbv.backgroundColor = UIColor.clear
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        super.viewDidLayoutSubviews()
+        
+        gradient.frame = tbv.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 12
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tbv.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 
 
 }
