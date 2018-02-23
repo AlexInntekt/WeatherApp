a68bf15fab43dbea75f2e9ab9ee773e4a4fb0a02
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-23 17:02:19 +0200

    Started to work on the UI

--- Paguda/ViewController.swift
+++ Paguda/ViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet var topView: UIView!
+    
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topView.layer.cornerRadius = topView.frame.size.height / 10
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
